5190afbcde8764324971e7e43655e4183576a899
Daniil Zatiaev <d.zatiaev@altsoft.spb.ru>
2023-02-15 18:46:22 +0300

    Test-commit

--- app/src/main/java/hexlet/Utils.java
+++ app/src/main/java/hexlet/Utils.java
@@ -44,11 +44,13 @@ public class Utils {
     public static boolean isPrime(int n) {
         int count = 0;
 
-        if (n < 2)
+        if (n < 2) {
             return false;
+        }
         for (int i = 2; i < Math.sqrt(n); i++) {
-            if (n % i == 0)
+            if (n % i == 0) {
                 return false;
+            }
         }
 
         return true;
