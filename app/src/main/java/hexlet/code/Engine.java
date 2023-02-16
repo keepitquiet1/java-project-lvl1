6559dacf41fca685a309ff32b3b243ee63ed9b3d
David <david.klesch@gmail.com>
2023-02-16 18:58:33 +0300

    Format fixes

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,18 +4,18 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    private static int score = 0;
     private static String name;
     public static final int ROUNDS_COUNT = 3;
 
-    public static void run(String DESCRIPTION, String[][] roundsData) {
+    public static void run(String description, String[][] roundsData) {
         Scanner scanner = new Scanner(System.in);
         getName(scanner);
         for (int i = 0; i < roundsData.length; i++) {
-            System.out.println(DESCRIPTION);
+            System.out.println(description);
             System.out.println(roundsData[i][0]);
-            if (!scanner.nextLine().equalsIgnoreCase(roundsData[i][1])){
-                System.out.printf("Incorrect! The correct answer is %s. Lets try again, %s! %n", roundsData[i][1], name);
+            if (!scanner.nextLine().equalsIgnoreCase(roundsData[i][1])) {
+                System.out.printf("Incorrect! The correct answer is %s. Lets try again, %s! %n",
+                        roundsData[i][1], name);
                 return;
             }
             System.out.println("Correct!");
